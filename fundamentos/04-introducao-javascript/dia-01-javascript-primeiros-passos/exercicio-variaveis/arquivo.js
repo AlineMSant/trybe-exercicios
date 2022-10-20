81ba5d6ff8e7f3b9abe9f23748d34fbb87068363
AlineMSant <alinems4120@gmail.com>
2022-10-20 15:26:02 -0300

    conslusão exercicio sobre variaveis

--- fundamentos/04-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio-variaveis/arquivo.js
+++ fundamentos/04-introducao-javascript/dia-01-javascript-primeiros-passos/exercicio-variaveis/arquivo.js
@@ -2,6 +2,10 @@ const myName = "Aline";
 const birthCity = "São Paulo";
 let birthYear = 1996;
 
+birthYear = 2030;
+// não atera por não ser variavel e sim uma constante.
+birthCity = "Minas Gerais" 
+
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
