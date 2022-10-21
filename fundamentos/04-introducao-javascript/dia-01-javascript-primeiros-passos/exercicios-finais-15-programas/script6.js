6b4d3443a0b18aefd8dca8d9c553abc6901e7bc3
AlineMSant <alinems4120@gmail.com>
2022-10-21 14:11:50 -0300

    exercicios

--- fundamentos/04-introducao-javascript/dia-01-javascript-primeiros-passos/exercicios-finais-15-programas/script6.js
+++ fundamentos/04-introducao-javascript/dia-01-javascript-primeiros-passos/exercicios-finais-15-programas/script6.js
@@ -1,14 +1,14 @@
-// const custo = 100;
-// const valorVenda = 100;
-// let valorCustoTotal = custo + (custo/100*20);
-// let quantidade = 1000;
-// let lucro;
+const custo = 100;
+const valorVenda = 100;
+let valorCustoTotal = custo + (custo/100*20);
+let quantidade = 1000;
+let lucro;
 
 
-// if(custo < 0 || valorVenda < 0){
-//     console.log("ERRO! Você deve inserir valores para custo e volerVenda");
-// }else{
-//     lucro = (valorVenda - valorCustoTotal) * quantidade;
-// }
+if(custo < 0 || valorVenda < 0){
+    console.log("ERRO! Você deve inserir valores para custo e volerVenda");
+}else{
+    lucro = (valorVenda - valorCustoTotal) * quantidade;
+}
 
-// console.log(lucro);
+console.log(lucro);
