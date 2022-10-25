b2a9fdd82116ba7de192b8af7672234f3a47c761
AlineMSant <alinems4120@gmail.com>
2022-10-25 18:12:28 -0300

    exercicio 5 programas com function

---
+++ fundamentos/04-introducao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio-function-5-programas.js
@@ -0,0 +1,28 @@
+// Cinco Programas
+
+function adicao(a,b){
+    return a+b;
+}
+
+
+function subtracao(a,b){
+    return a-b;
+}
+
+function multiplicacao(a,b){
+    return a*b;
+}
+
+function divisao(a,b){
+    return a/b;
+}
+
+function modulo(a,b){
+    return a%b;
+}
+
+console.log(adicao(20,5));
+console.log(subtracao(20,5));
+console.log(multiplicacao(20,5));
+console.log(divisao(20,5));
+console.log(modulo(20,5));
