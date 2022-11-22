6e0c73d992c6594a3c544cbafba21241e6af9a62
AlineMSant <alinems4120@gmail.com>
2022-11-22 18:46:48 -0300

    exercicio 3

--- fundamentos/07-introducao-javascript-ES6-testes-unitarios/dia-03-matchers-cobertura-codigos/printMessage.test.js
+++ fundamentos/07-introducao-javascript-ES6-testes-unitarios/dia-03-matchers-cobertura-codigos/printMessage.test.js
@@ -3,3 +3,7 @@ const { info, printMessage } = require('./printMessage');
 test('confere se printMessage recebe objeto que possui personagem', () => {
     expect(info).toHaveProperty('personagem');
 });
+
+test('confere se printMessage contem Boas Vindas', () => {
+    expect(printMessage(info)).toMatch('Boas vindas, Margarida');
+})
