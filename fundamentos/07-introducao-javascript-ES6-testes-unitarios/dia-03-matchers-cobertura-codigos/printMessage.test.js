787c3ae606fea22d2711f11de8e6dfd675a02cf5
AlineMSant <alinems4120@gmail.com>
2022-11-22 18:59:45 -0300

    exercicio 4

--- fundamentos/07-introducao-javascript-ES6-testes-unitarios/dia-03-matchers-cobertura-codigos/printMessage.test.js
+++ fundamentos/07-introducao-javascript-ES6-testes-unitarios/dia-03-matchers-cobertura-codigos/printMessage.test.js
@@ -7,3 +7,15 @@ test('confere se printMessage recebe objeto que possui personagem', () => {
 test('confere se printMessage contem Boas Vindas', () => {
     expect(printMessage(info)).toMatch('Boas vindas, Margarida');
 })
+
+describe('testa Erro', () => {
+    it('testa se lança mensagem de erro', () => {
+        expect(()=> {printMessage()}).toThrow();
+    })
+    it('teste se retorna erro quando não seja objeto', () => {
+        expect(()=> {printMessage(1)}).toThrow('objeto inválido');
+    })
+    it('teste se retorna erro quando não possui a propriedade personagem', () => {
+        expect(()=> {printMessage({origem: 'Pato Donald'})}).toThrow('objeto inválido');
+    })
+})
